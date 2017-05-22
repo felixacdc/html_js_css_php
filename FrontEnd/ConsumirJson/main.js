6bcc4c7e83cbef9d0639e672b15526acfe1acb09
felixacdc <pfmata01@gmail.com>
2017-05-22 13:55:25 -0600

    [add] Consumir Json con JavaScript puro

--- FrontEnd/ConsumirJson/main.js
+++ FrontEnd/ConsumirJson/main.js
@@ -1,5 +1,34 @@
+function leerConJavaScript() {
+	let xmlhttp = new XMLHttpRequest();
+	let alumnos = document.getElementById("contentAlumnos");
+	
+	xmlhttp.onreadystatechange = function() {
+		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+		    var jsonResponse = xmlhttp.responseText;
+		    var objeto_json = JSON.parse(jsonResponse);
+		 
+		    for (var i=0; i < objeto_json.length; i++) {
+		    	var tr = document.createElement('tr');
+		    	var tdNombre = document.createElement('td');
+		    	var tdEdad = document.createElement('td');
+		    	var tdSexo = document.createElement('td');
+			    tdNombre.innerHTML = objeto_json[i].nombre;
+			    tdEdad.innerHTML = objeto_json[i].edad;
+			    tdSexo.innerHTML = objeto_json[i].sexo;
+			    tr.appendChild(tdNombre);
+			    tr.appendChild(tdEdad);
+			    tr.appendChild(tdSexo);
+			    alumnos.appendChild(tr);
+		    }	    
+		}
+	}
+
+	xmlhttp.open("GET","alumnos.json");
+	xmlhttp.send();
+}
+
 function leerConJquery() {
-	$.getJSON("./maestros.json", function(datos) {
+	$.getJSON("maestros.json", function(datos) {
         $.each(datos, function(index, valor) {
         	$("#contentMaestros tbody").append(`<tr><td>${valor['nombre']}</td><td>${valor['edad']}</td><td>${valor['profesion']}</td></tr>`);
         });
@@ -24,5 +53,6 @@ function leerConVue() {
 	})
 }
 
+leerConJavaScript();
 leerConJquery();
 leerConVue();
